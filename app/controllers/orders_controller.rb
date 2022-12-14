3bf10ae2bdaf0b7ff787b78e4aa116104656ca9f
Francisco <kikopocampo@gmail.com>
2022-12-14 10:49:17 -0700

    implement order summery

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @item = LineItem.where(id: @order.line_items.pluck(:id))
   end
 
   def create
